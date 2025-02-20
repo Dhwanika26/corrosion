c1f83d812194273e3f08807923a53e076a214dcf
Dhwanika <singhdhwanika@gmail.com>
2025-02-20 14:13:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+// page.tsx
+import type { Metadata } from 'next';
 import { Hero } from "@/sections/Hero";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Features } from "@/sections/Features";
 import { FAQs } from "@/sections/FAQs";
 
+export const metadata: Metadata = {
+  title: "My Page",
+  description: "This is a page description",
+};
+
 export default function Home() {
   return (
-  <>
-    <Hero/>
-    <ProductShowcase/>
-    <Features/> 
-    <FAQs/>
-  </>
+    <>
+      <Hero />
+      <ProductShowcase />
+      <Features />
+      <FAQs />
+    </>
   );
 }
